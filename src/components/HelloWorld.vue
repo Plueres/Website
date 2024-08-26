9fce13d2d0d69eb49a0f6e7ce37e97e32aca99b2
Rick Slierendregt <s1204255@student.windesheim.nl>
2024-08-27 00:17:23 +0200

    Updated helloworld file for testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,6 +42,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 h3 {
   margin: 40px 0 0;
 }
