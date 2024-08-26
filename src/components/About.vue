6ddcbd811f7df7f58fedad93297b2b45f651ed97
Rick Slierendregt <113343182+Pluers@users.noreply.github.com>
2024-08-26 12:43:29 -0700

    Added about page

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>
+        What Should Be On a Web Developer Portfolio?
+        Include at least these in your web developer portfolio:
+        – Contact information, including relevant social media accounts
+        – Short bio and your photo
+        – Relevant experience and skills with context
+        – Personal projects
+        – Documented source code
+        – Education
+        – Awards/recognition
+        – Downloadable web developer resume
+    </p>
+</template>
